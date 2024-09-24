905441525e65497786a2d6a86eff01a487c746ec
brian <yangbri1@msu.edu>
2024-09-24 15:16:06 -0400

    wireframing before coding -- checking other websites for inspirations

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// let div = document.createElement('div');
+// div.id = 'content';
+// div.innerHTML = '<p>CreateElement example</p>';
+
+// document.body.appendChild(div);
+
+// BOM block is indepedent from DOM & would run before the DOM
+window.alert("Due to insufficient funding, we're broke")
